ed8c3842f0db01cc3fc262f949ac299b28e4c52e
admin <bling9356@gmail.com>
2024-06-07 17:05:47 +0900

    "바뀐 내용"

--- src/main/java/com/example/firstproject/repository/CommentRepository.java
+++ src/main/java/com/example/firstproject/repository/CommentRepository.java
@@ -1,16 +1,18 @@
 package com.example.firstproject.repository;
 
-import com.example.firstproject.entity.Comment;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import com.example.firstproject.entity.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     // 특정 게시글의 모든 댓글 조회
     @Query(value = "SELECT * FROM comment WHERE article_id = :articleId",
             nativeQuery = true) // value 속성에 실행하려는 쿼리 작성
-    List<Comment> findByArticleId(Long articleId);
+    List<Comment> findByArticleId(@Param("articleId") Long articleId);
     // 특정 닉네임의 모든 댓글 조회
     List<Comment> findByNickname(String nickname);
 }
